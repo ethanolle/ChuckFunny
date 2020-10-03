b1577401a36b07dea87d1e642b66dc01e6a6b536
ethan sayagh <ethan.sayagh@gmail.com>
2020-10-03 11:51:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Chuck from "./gettyimages-83457444-612x612.jpg";
+import axios from "axios";
 
 function App() {
+  const [state, setState] = useState({
+    joke: "",
+    searchKeyword: "",
+    searchUrl: "https://api.chucknorris.io/jokes/search?query=",
+  });
+
+  useEffect(() => {
+    fetchData();
+    // estlint-disable-next-line react-hooks/exhaust
+  }, []);
+
+  const fetchData = async () => {
+    const result = await axios.get("https://api.chucknorris.io/jokes/random");
+    console.log(result.data.value);
+    setState({ ...state, joke: result.data.value });
+  };
+
+  const searchJoke = (event) => {
+    setState({ ...state, searchKeyword: event.target.value });
+    console.log(event.target.value);
+  };
+
+  const fetchMyJoke = async () => {
+    const result = await axios.get(state.searchUrl + state.searchKeyword);
+    console.log(result.data.result[0].value);
+    setState({ ...state, joke: result.data.result[0].value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="title">Chuck API</h1>
+          <img src={Chuck} alt="ChuckNoris" />
+        </div>
+        <div className="col-6 searchJokeCol">
+          <div className="card">
+            <div className="card-header">
+              <span>Search for the Phrase of Chuck</span>
+            </div>
+            <div className="card-body">
+              <input type="text" onChange={searchJoke}></input>
+            </div>
+          </div>
+          <div>
+            <button onClick={fetchMyJoke} className="btn btn-warning btn-lg">
+              Just CLICK!
+            </button>
+          </div>
+        </div>
+      </div>
+      <h2 className="subTitle"> Here is The joke</h2>
+      <h4>{state.joke}</h4>
     </div>
   );
 }
