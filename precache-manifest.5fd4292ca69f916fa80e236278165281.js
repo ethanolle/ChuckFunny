766ac73a0864cfcdf8b888a7cee6de60bcf10ee6
ethan sayagh <ethan.sayagh@gmail.com>
2020-10-03 14:37:49 +0300

    Updates

---
+++ precache-manifest.5fd4292ca69f916fa80e236278165281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0482ceca3594a1b8277547f9eb2976",
+    "url": "/ChuckFunny/index.html"
+  },
+  {
+    "revision": "35d5a14bbf3b8c5dc4aa",
+    "url": "/ChuckFunny/static/css/main.dc974f97.chunk.css"
+  },
+  {
+    "revision": "776ba3395cd5d6c31cf1",
+    "url": "/ChuckFunny/static/js/2.303a4f3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChuckFunny/static/js/2.303a4f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d5a14bbf3b8c5dc4aa",
+    "url": "/ChuckFunny/static/js/main.887ae2ee.chunk.js"
+  },
+  {
+    "revision": "3e27d8158fa91b95ab38",
+    "url": "/ChuckFunny/static/js/runtime-main.f240f038.js"
+  },
+  {
+    "revision": "592c1c35f711390466d78b17118c5630",
+    "url": "/ChuckFunny/static/media/gettyimages-83457444-612x612.592c1c35.jpg"
+  }
+]);
